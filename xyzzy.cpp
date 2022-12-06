d9a940bd885abcc736d0b31e98aca61f600bcd11
Vrinda-0710 <117075081+Vrinda-0710@users.noreply.github.com>
2022-12-06 16:35:15 +0530

    Update xyzzy.cpp

--- xyzzy.cpp
+++ xyzzy.cpp
@@ -1,1 +1,28 @@
-hsbckjwebc
+//LINEAR SEARCH
+#include <iostream>
+using namespace std;
+
+int linearsearch(int arr[],int n,int key){
+    for(int i=0;i<n;i++){
+        if(arr[i]==key){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[]={10,15,12,9,6,4,7,0,34};
+    int n=sizeof(arr)/sizeof(int);
+    
+    int key;
+    cin>>key;
+    
+    int index=linearsearch(arr,n,key);
+    if(index!=-1){
+        cout<<key<<" is present in array at index "<<index<<endl;
+    }
+    else{
+        cout<<key<<" is not found"<<endl;
+    }
+}
