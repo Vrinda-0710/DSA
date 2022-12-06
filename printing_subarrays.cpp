bfd2a3ed200a88192db2b7b9f504edc59dca0185
Vrinda-0710 <117075081+Vrinda-0710@users.noreply.github.com>
2022-12-06 17:38:17 +0530

    Create printing_subarrays.cpp

---
+++ printing_subarrays.cpp
@@ -0,0 +1,27 @@
+//PRINT SUB ARRAYS
+#include<iostream>
+using namespace std;
+
+void  printsubarrays(int arr[], int n){
+    
+    for (int i=0;i<n;i++){
+        for (int j=i;j<n;j++){
+            for (int k=i;k<j;k++){
+                cout<<arr[k]<<",";
+            }
+            cout<<endl;
+        }
+    }
+    
+    
+}
+
+int main(){
+    int arr[]={10,20,30,40,45,60,70,89};
+    int n=sizeof(arr)/sizeof(int);
+    printsubarrays(arr,n);
+    
+    return 0;
+    
+    
+}
